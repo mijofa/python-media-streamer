dfdabd54fe2df3a140a88dfa7427ad88775b9376
Mike Abrahall <mike@cyber.com.au>
2018-08-15 12:24:41 +1000

    Set media path to current directory, appease flake8

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 import flask
-import json
-import subprocess
 import os.path
-import time
 
 import ffmpeg
 
 app = flask.Flask("python-media-streamer")
 
-@app.route('/')
-def index():
-  return "Indexing isn't supported yet"
 
-@app.route('/watch/<path:filename>/player.html')
-def watch(filename):
-    filepath = '/srv/media/Video/' + filename
+def get_mediauri(filename):
+    filepath = os.path.join(os.path.abspath(os.path.curdir), filename)
     assert os.path.exists(filepath) and not os.path.isdir(filepath)
     fileuri = 'file:' + filepath
     del filename
     del filepath
 
+    return fileuri
+
+
+@app.route('/')
+def index():
+    return "Indexing isn't supported yet"
+
+
+@app.route('/watch/<path:filename>/player.html')
+def watch(filename):
     return """<!doctype html><html>
         <head><script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script></head>
         <body>
@@ -28,7 +31,10 @@ def watch(filename):
             <script>
                 var video = document.getElementById("video");
                 if(Hls.isSupported()) {
-                  var hls = new Hls({maxBufferLength: 500, manifestLoadingTimeOut: 20000, levelLoadingTimeOut: 20000, fragLoadingTimeOut: 120000});
+                  var hls = new Hls({maxBufferLength: 500,
+                                     manifestLoadingTimeOut: 20000,
+                                     levelLoadingTimeOut: 20000,
+                                     fragLoadingTimeOut: 120000});
                   hls.loadSource("manifest.m3u8");
                   hls.attachMedia(video);
                   hls.on(Hls.Events.MANIFEST_PARSED,function() {
@@ -39,26 +45,18 @@ def watch(filename):
         </body>
     </html>"""
 
+
 @app.route('/watch/<path:filename>/manifest.m3u8')
 def manifest(filename):
-    # FIXME: Don't copy-paste this shit
-    filepath = '/srv/media/Video/' + filename
-    assert os.path.exists(filepath) and not os.path.isdir(filepath)
-    fileuri = 'file:' + filepath
-    del filename
-    del filepath
+    fileuri = get_mediauri(filename)
 
     duration = ffmpeg.probe(fileuri)['container']['duration']
     return flask.Response(ffmpeg.generate_manifest(duration), mimetype='application/x-mpegURL')
 
+
 @app.route('/watch/<path:filename>/hls-segment.ts')
 def hls_segment(filename):
-    # FIXME: Don't copy-paste this shit
-    filepath = '/srv/media/Video/' + filename
-    assert os.path.exists(filepath) and not os.path.isdir(filepath)
-    fileuri = 'file:' + filepath
-    del filename
-    del filepath
+    fileuri = get_mediauri(filename)
 
     # FIXME: Assert that there's no more than one of each argument
     return flask.Response(ffmpeg.get_segment(fileuri=fileuri,
@@ -66,6 +64,7 @@ def hls_segment(filename):
                                              offset=float(flask.request.args['offset']),
                                              length=float(flask.request.args['length'])),
                           mimetype='video/mp2t')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
